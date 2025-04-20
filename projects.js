54c22b7586114a1c85a6c90732b6f34df71befdd
Matthew <mateoalva7984@gmail.com>
2025-04-20 01:38:59 -0400

    changed placeholder and made projects a global variable

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-export const projectsInfo = [
+ const projectsInfo = [
     {
         projectName: 'Some project',
         projectImage: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -28,4 +28,6 @@ export const projectsInfo = [
         projectDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ',
         projectLink: 'https://github.com/',
     }
-]
+];
+
+window.projectsInfo = projectsInfo;
--- script.js
+++ script.js
@@ -1,24 +1,32 @@
+// import the projects from the js file and display them in the html file
+   // import { projectsInfo } from './projects.js';
+
+    console.log(window.projectsInfo);
+
+
 const leftButton = document.getElementById('left');
 const rightButton = document.getElementById('right');
 
 leftButton.addEventListener('click', () => {
     console.log('Left button clicked');
     const leftButton = document.getElementById('left');
+    /*
     leftButton.style.backgroundColor = 'blue';
     leftButton.style.color = 'white';
     leftButton.style.borderRadius = '5px';
     leftButton.style.padding = '10px';
     leftButton.style.fontSize = '16px';
-
+    */
 })
 rightButton.addEventListener('click', () => {
     console.log('Right button clicked');
     const rightButton = document.getElementById('right');
+    /*
     rightButton.style.backgroundColor = 'green';
     rightButton.style.color = 'white';
     rightButton.style.borderRadius = '5px';
     rightButton.style.padding = '10px';
     rightButton.style.fontSize = '16px';
-
+    */
 
 })
